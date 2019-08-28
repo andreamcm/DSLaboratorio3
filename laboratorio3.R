b11ee3499ccc148e79d34919a6e49b1cd1716a8d
andreamcm <cor16076@uvg.edu.gt>
2019-08-28 12:20:44 -0600

    Inicial

---
+++ laboratorio3.R
@@ -0,0 +1,19 @@
+# Universidad del Valle de Guatemala
+# Ivette Cardona - 16020
+# Andrea María Cordón - 16076
+
+
+
+
+# Set del directorio donde estan los datos
+# -----------------------------------------
+# Andrea
+setwd("~/2019/UVG/Segundo Semestre/DataScience/Laboratorios/Laboratorio3/DSLaboratorio3")
+# Ivette
+setwd("C:/Users/DELL/Documents/UVG/VIII_Semestre/Data Science/DSLaboratorio2")
+
+
+
+# --------------------
+# Librerias a utilizar
+# --------------------
