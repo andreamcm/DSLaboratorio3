05efd10c02f11a548e3db968dbc1a6fa5e4a7441
Ivette <car16020@uvg.edu.gt>
2019-09-02 11:48:20 -0600

    Cambios

--- series.R
+++ series.R
@@ -66,6 +66,7 @@ pp.test(count_d1, alternative="stationary")
 
 #Nuevo grafico
 plot(decompose(count_d1))
+plot(count_d1)
 
 #MODELO ARIMA
 ndiffs(count_d1)
@@ -85,63 +86,12 @@ auto.arima(count_d1, trace=TRUE)
 
 modeloarimadiesel<-auto.arima(count_d1, seasonal=FALSE)
 modeloarimadiesel
-tsdisplay(residuals(modeloarimadiesel), lag.max=10, main='(3,1,1) Diesel Model Residuals')
+tsdisplay(residuals(modeloarimadiesel), lag.max=10, main='(1,1,1)(1,0,0) Diesel Model Residuals')
 
-#Prediccion 2020
+#Prediccion 2019
 predict(fitARIMAdl,n.ahead = 5)
-futurValdl <- forecast(fitARIMAdl,h=10, level=c(99.5))
+futurValdl <- forecast(fitARIMAdl,h=35, level=c(95))
 plot(futurValdl)
-
-########################################################
-############ GASOLINA REGULAR   ########################
-########################################################
-
-#Transformamos los datos en una serie temporal 
-datos_regular<-ts(datos$GasRegular, start = c(2001,1), frequency = 12)
-print(datos_regular)
-
-#Trazamos la serie de tiempo datos_diesel
-autoplot(datos_regular, ts.colour = "blue", ts.linetype = "dashed", xlab = "Time", ylab = "Diesel",
-         title = "Regular Gas behavior")
-
-#Descomposicion
-plot(decompose(datos_regular))
-autoplot(stl(datos_regular, s.window = "periodic"), ts.colour = "blue")
-
-#Autocorrelacion 
-acf(datos_regular)
-
-#Verifiacion de varianza
-autoplot(acf(datos_regular, plot = FALSE))
-
-#Pruebas
-adf.test(diff(log(datos_regular)), alternative="stationary", k=0)
-pp.test(diff(log(datos_regular), alternative="stationary"))
-
-#Nuevo grafico
-plot(decompose(diff(log(datos_regular))))
-
-#MODELO ARIMA
-ndiffs(datos_regular)
-nsdiffs(datos_regular)
-
-fitARIMAreg <- arima(datos_regular, order=c(1,1,1),seasonal = list(order = c(1,0,0), period = 12),method="ML")
-coeftest(fitARIMAreg)
-confint(fitARIMAreg)
-
-acf(fitARIMAreg$residuals,lag.max=140)
-boxresult=LjungBoxTest (fitARIMAreg$residuals,k=2,StartLag=1)
-plot(boxresult[,3],main= "Ljung-Box Q Test", ylab= "P-values", xlab= "Lag")
-qqnorm(fitARIMAreg$residuals)
-qqline(fitARIMAreg$residuals)
-
-auto.arima(datos_regular, trace=TRUE)
-
-#Prediccion 2020
-predict(fitARIMAreg,n.ahead = 5)
-futurValdl <- forecast(fitARIMAreg,h=10, level=c(99.5))
-plot(futurValdl)
-
 
 
 ########################################################
